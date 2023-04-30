a2a42b247528ab5281540566d5f508482d492d63
Boclair Temgoua <temgoua2000@gmail.com>
2023-04-30 18:19:06 +0200

    fix(general): fix project

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,15 @@ async function bootstrap() {
   // config.update({});
   const port = configurations.port;
   const version = configurations.api.version;
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(`/api/${version}`);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
-  app.use(helmet());
-  app.enableCors();
+  // app.use(helmet());
   // app.use(useragent.express());
   await app.listen(port, () => {
     console.log(`=============================================`);
