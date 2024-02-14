66466388eee9fc716b8927fc7c7cdf9436a66fa3
Boclair Temagoua <boclair.temgoua@recrowd.com>
2024-02-14 02:06:14 +0100

    fix(module): fix http

--- src/app/utils/cookies.ts
+++ src/app/utils/cookies.ts
@@ -9,8 +9,14 @@ export const validation_code_verification_cookie_setting = {
 export const validation_login_cookie_setting = {
   maxAge: Number(config.cookie_access.accessExpire),
   httpOnly: false,
+  secure: true,
+  // domain: '',
+  sameSite: 'None',
 };
 
 export const expire_cookie_setting = {
   httpOnly: true,
+  secure: true,
+  // domain: '',
+  // sameSite: '',
 };
