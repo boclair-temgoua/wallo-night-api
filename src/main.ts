9990ce2e9bd9568f4a86867c96ec1ed2b02ffb38
Boclair Temagoua <boclair.temgoua@recrowd.com>
2024-02-14 10:45:45 +0100

    fix(module): fix bug

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
   app.enableCors({
     origin: whitelist,
     credentials: true,
+    exposedHeaders: ['Set-cookie'],
   });
   app.use(cookieParser());
   app.useGlobalPipes(
