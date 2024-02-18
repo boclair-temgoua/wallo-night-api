ef878f71e090672528572be9e7468d19c88399c1
Boclair Temagoua <boclair.temgoua@recrowd.com>
2024-02-18 16:48:37 +0100

    fix(module): fix database

--- src/app/databases/migrations/1707744836593-migration.ts
+++ src/app/databases/migrations/1707744836593-migration.ts
@@ -1,18 +1,25 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1707744836593 implements MigrationInterface {
-    name = 'Migration1707744836593'
+  name = 'Migration1707744836593';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "transaction" ADD "orderId" uuid`);
-        await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "UQ_a6e45c89cfbe8d92840266fd30f" UNIQUE ("orderId")`);
-        await queryRunner.query(`ALTER TABLE "transaction" ADD CONSTRAINT "FK_a6e45c89cfbe8d92840266fd30f" FOREIGN KEY ("orderId") REFERENCES "order"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "transaction" ADD "orderId" uuid`);
+    await queryRunner.query(
+      `ALTER TABLE "transaction" ADD CONSTRAINT "UQ_a6e45c89cfbe8d92840266fd30f" UNIQUE ("orderId")`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transaction" ADD CONSTRAINT "FK_a6e45c89cfbe8d92840266fd30f" FOREIGN KEY ("orderId") REFERENCES "order"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "FK_a6e45c89cfbe8d92840266fd30f"`);
-        await queryRunner.query(`ALTER TABLE "transaction" DROP CONSTRAINT "UQ_a6e45c89cfbe8d92840266fd30f"`);
-        await queryRunner.query(`ALTER TABLE "transaction" DROP COLUMN "orderId"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "transaction" DROP CONSTRAINT "FK_a6e45c89cfbe8d92840266fd30f"`,
+    );
+    await queryRunner.query(
+      `ALTER TABLE "transaction" DROP CONSTRAINT "UQ_a6e45c89cfbe8d92840266fd30f"`,
+    );
+    await queryRunner.query(`ALTER TABLE "transaction" DROP COLUMN "orderId"`);
+  }
 }
