71c48224b7901ec45493cdbf18aab5fb04477787
Boclair Temagoua <boclair.temgoua@recrowd.com>
2024-02-27 17:45:55 +0100

    fix(module): fix service

--- src/modules/uploads/uploads.util.ts
+++ src/modules/uploads/uploads.util.ts
@@ -15,6 +15,8 @@ const fieldnameLists: KeyAsString = {
   attachmentImages: 'IMAGE',
   attachmentFiles: 'FILE',
 };
+
+type ExpressFile = Express.Multer.File;
 @Injectable()
 export class UploadsUtil {
   constructor(private readonly uploadsService: UploadsService) {} // private driver: Repository<Commission>, // @InjectRepository(Commission)
@@ -30,9 +32,11 @@ export class UploadsUtil {
     membershipId?: string;
 
     folder: 'products' | 'commissions' | 'posts' | 'memberships';
-    files: Array<Express.Multer.File>;
+    files: Array<ExpressFile>;
+    file?: ExpressFile;
   }): Promise<any> {
     const {
+      file,
       files,
       userId,
       organizationId,
@@ -45,40 +49,96 @@ export class UploadsUtil {
       membershipId,
     } = options;
 
-    for (const file of files) {
-      const extension = mime.extension(file.mimetype);
-      const nameFile = `${Slug(file?.originalname)}${formateNowDateYYMMDD(
-        new Date(),
-      )}-${generateLongUUID(4)}`;
-      const fileName = `${`${nameFile}.${
-        extension === 'mpga' ? 'mp3' : extension
-      }`}`;
-
-      const urlAWS = await awsS3ServiceAdapter({
-        fileName: fileName,
-        mimeType: file?.mimetype,
-        folder: folder,
-        file: file.buffer,
-      });
-
-      await this.uploadsService.createOne({
-        name: file?.originalname,
-        path: fileName,
-        size: Number(file?.size),
-        status: 'success',
-        url: urlAWS.Location,
-        uploadType: fieldnameLists[file?.fieldname] as UploadType,
-        model: model,
-        userId: userId,
-        postId: postId,
-        productId: productId,
-        commissionId: commissionId,
-        membershipId: membershipId,
-        organizationId: organizationId,
-        uploadableId: uploadableId,
+    if (file) {
+      await this.uploadOne({
+        file,
+        folder,
+        model,
+        userId,
+        postId,
+        productId,
+        commissionId,
+        membershipId,
+        organizationId,
+        uploadableId,
       });
     }
 
+    if (files.length > 0) {
+      for (const file of files) {
+        await this.uploadOne({
+          file,
+          folder,
+          model,
+          userId,
+          postId,
+          productId,
+          commissionId,
+          membershipId,
+          organizationId,
+          uploadableId,
+        });
+      }
+    }
+
     return 'ok';
+  }
+
+  async uploadOne(options: {
+    file: ExpressFile;
+    folder: string;
+    model: FilterQueryType;
+    userId: string;
+    postId: string;
+    productId: string;
+    commissionId: string;
+    membershipId: string;
+    organizationId: string;
+    uploadableId: string;
+  }) {
+    const {
+      file,
+      folder,
+      model,
+      userId,
+      postId,
+      productId,
+      commissionId,
+      membershipId,
+      organizationId,
+      uploadableId,
+    } = options;
+
+    const extension = mime.extension(file.mimetype);
+    const nameFile = `${Slug(file?.originalname)}${formateNowDateYYMMDD(
+      new Date(),
+    )}-${generateLongUUID(4)}`;
+    const fileName = `${`${nameFile}.${
+      extension === 'mpga' ? 'mp3' : extension
+    }`}`;
+
+    const urlAWS = await awsS3ServiceAdapter({
+      fileName: fileName,
+      mimeType: file?.mimetype,
+      folder: folder,
+      file: file.buffer,
+    });
+
+    await this.uploadsService.createOne({
+      name: file?.originalname,
+      path: fileName,
+      size: Number(file?.size),
+      status: 'success',
+      url: urlAWS.Location,
+      uploadType: fieldnameLists[file?.fieldname] as UploadType,
+      model: model,
+      userId: userId,
+      postId: postId,
+      productId: productId,
+      commissionId: commissionId,
+      membershipId: membershipId,
+      organizationId: organizationId,
+      uploadableId: uploadableId,
+    });
   }
 }
