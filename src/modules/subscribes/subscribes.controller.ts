f94a1c37290f1db3c12df89e0fe8c04e33c79bcf
Boclair Temagoua <boclair.temgoua@recrowd.com>
2024-03-14 04:12:54 +0100

    fix8module): fix bug

--- src/modules/subscribes/subscribes.controller.ts
+++ src/modules/subscribes/subscribes.controller.ts
@@ -25,7 +25,6 @@ import {
 } from '../../app/utils/pagination/with-pagination';
 import { SearchQueryDto } from '../../app/utils/search-query/search-query.dto';
 import { FollowsService } from '../follows/follows.service';
-import PayPalIntegration from '../integrations/paypal/paypal.integration';
 import { MembershipsService } from '../memberships/memberships.service';
 import { TransactionsService } from '../transactions/transactions.service';
 import { SubscribesService } from './subscribes.service';
@@ -176,15 +175,5 @@ export class SubscribesController {
     }
 
     return reply({ res, results: 'User Subscribe successfully' });
-  }
-
-  @Post(`/paypal-test`)
-  @UseGuards(UserAuthGuard)
-  async createPayPalOne(@Res() res, @Req() req) {
-    const paypal = new PayPalIntegration();
-    const data = await paypal.getAccessToken();
-    // const data = await getPayPalAccessToken();
-
-    return reply({ res, results: data });
   }
 }
